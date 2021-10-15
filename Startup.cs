afd4222a646816d3954835b9d5ccb401ccafb313
RadityaPratama03 <RadityaPratama03>
2021-10-15 21:07:34 +0700

    Raditya Pratama Putra_20190140101

--- Startup.cs
+++ Startup.cs
@@ -53,7 +53,7 @@ namespace Employee
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Employees}/{action=Index}/{id?}");
             });
         }
     }
